08b2063f9fb798309a29e85f15f46cbef00865d8
James Houghton <jamesthoughton@gmail.com>
2017-02-18 16:09:22 -0500

    Confirm it works -- display the video frame by frame

--- ttyvideo.c
+++ ttyvideo.c
@@ -1,6 +1,7 @@
 #include <opencv2/core/core_c.h>
-#include <opencv2/videoio/videoio_c.h>
+// #include <opencv2/videoio/videoio_c.h>
 #include <opencv2/imgproc/imgproc_c.h>
+#include <opencv2/highgui/highgui_c.h>
 #include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -15,10 +16,50 @@ int main(int argc, char** argv) {
 	CvCapture* cap = cvCaptureFromFile(filename);
 	if(!cap) return error("Can't read input");
 
-	IplImage* frame = cvQueryFrame(cap);
+	register int width, height, nchannels, step, offset;
+	register int i, j, r_ch, b_ch, g_ch;
 
 	double fps = (double)cvGetCaptureProperty(cap, CV_CAP_PROP_FPS);
 
-	printf("%f\n", fps);
+	register int frameNum = 0;
+
+	IplImage* frame;
+
+   	frame = cvQueryFrame(cap);
+
+	cvNamedWindow("w", 1);
+
+	while(frame != NULL) {
+
+		width = frame->width;
+		height = frame->height;
+		nchannels = frame->nChannels;
+		step = frame->widthStep;
+
+		for(i = 0; i < height; ++i) {
+			for(j = 0; j < width; ++j) {
+				unsigned char* data = (unsigned char*)(frame->imageData + i*step);
+				offset = j * nchannels;
+				b_ch = data[offset];
+				g_ch = data[offset+1];
+				r_ch = data[offset+2];
+			}
+		}
+
+		printf("Finished parsing frame %d\n", frameNum++);
+		printf("Final bits: %d %d %d\n", b_ch, g_ch, r_ch);
+
+		cvShowImage("w", frame);
+
+		cvWaitKey(1000/fps);
+
+		frame = cvQueryFrame(cap);
+
+	}
+
+	cvDestroyWindow("w");
+	cvReleaseImage(&frame);
+
+	return 0;
 
 }
