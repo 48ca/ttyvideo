ff37c138b10ddd6699c078c9a660bfabe255f624
James Houghton <jamesthoughton@gmail.com>
2017-02-18 19:53:18 -0500

    Remove unnecessary opencv includes

--- ttyvideo.c
+++ ttyvideo.c
@@ -1,7 +1,5 @@
 #include <opencv2/core/core_c.h>
 #include <opencv2/videoio/videoio_c.h>
-#include <opencv2/imgproc/imgproc_c.h>
-// #include <opencv2/highgui/highgui_c.h>
 #include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
